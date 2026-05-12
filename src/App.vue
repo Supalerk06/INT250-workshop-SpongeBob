61d112a8fe90d5c6070ace5d2c24a1d5a87d4660
Supalerk06 <supalerk.kamo06@gmail.com>
2026-05-12 14:12:39 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import Card from './components/Card.vue'
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<script setup>
+// defineProps({
+//   promotion: String
+// })
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
+
+<style scoped>
+</style>
